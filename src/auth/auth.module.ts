25254d0a9bb012c0ebcf3a6b6bb18e4f8b713ed0
Facundo Iguacel <iguacelfacundo@gmail.com>
2022-10-20 15:16:17 -0300

    remove unused google auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,22 +7,16 @@ import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 import { UsersModule } from '../users/users.module';
 
-import { GoogleController } from './google.controller';
-import { GoogleService } from './google.service';
-
 import { GoogleAuthenticationService } from './googleAuthentication.service';
-import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [UsersModule, JwtModule],
-  controllers: [AuthController, GoogleController],
+  controllers: [AuthController],
   providers: [
     AuthService,
     JwtService,
     AccessTokenStrategy,
     RefreshTokenStrategy,
-    GoogleStrategy,
-    GoogleService,
     GoogleAuthenticationService,
   ],
 })
