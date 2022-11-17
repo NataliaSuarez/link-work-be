3623f8141b33f48d15de5b3c1665dad743df2646
Matias Viano <matias.a.viano@gmail.com>
2022-11-17 16:24:16 -0300

    return decodedObj

--- src/auth/apple/apple.service.ts
+++ src/auth/apple/apple.service.ts
@@ -9,29 +9,30 @@ export class AppleService {
     //You can decode the id_token which returned from Apple,
     const decodedObj = await this.jwtService.decode(payload.id_token);
     const accountId = decodedObj.sub || '';
+    return decodedObj;
     //console.info(`Apple Account ID: ${accountId}`);
-    let email = '';
+    // let email = '';
 
-    //Email address
-    if (decodedObj.hasOwnProperty('email')) {
-      email = decodedObj['email'];
-      //console.info(`Apple Email: ${email}`);
-    }
+    // //Email address
+    // if (decodedObj.hasOwnProperty('email')) {
+    //   email = decodedObj['email'];
+    //   //console.info(`Apple Email: ${email}`);
+    // }
 
-    //You can also extract the firstName and lastName from the user, but they are only shown in the first time.
-    if (payload.hasOwnProperty('user')) {
-      const userData = JSON.parse(payload.user);
-      //const { firstName, lastName } = userData.name || {};
-      return {
-        accountId: accountId,
-        email: email,
-        userData: userData,
-      };
-    }
-    return {
-      accountId: accountId,
-      email: email,
-    };
+    // //You can also extract the firstName and lastName from the user, but they are only shown in the first time.
+    // if (payload.hasOwnProperty('user')) {
+    //   const userData = JSON.parse(payload.user);
+    //   //const { firstName, lastName } = userData.name || {};
+    //   return {
+    //     accountId: accountId,
+    //     email: email,
+    //     userData: userData,
+    //   };
+    // }
+    // return {
+    //   accountId: accountId,
+    //   email: email,
+    // };
     //.... you logic for registration and login here
   }
 }
