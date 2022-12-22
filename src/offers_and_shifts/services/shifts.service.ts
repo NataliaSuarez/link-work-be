5c91232f3ddccfd2e793e900cdc057db3d7a494f
Matias Viano <matias.a.viano@gmail.com>
2022-12-22 18:11:08 -0300

    resp shift id

--- src/offers_and_shifts/services/shifts.service.ts
+++ src/offers_and_shifts/services/shifts.service.ts
@@ -110,44 +110,7 @@ export class ShiftsService {
     } else {
       profileImg = null;
     }
-    const formatShift = {
-      id: shift.id,
-      clockIn: shift.clockIn,
-      confirmedClockIn: shift.confirmedClockIn,
-      clockOut: shift.clockOut,
-      confirmedClockOut: shift.confirmedClockOut,
-      status: shift.status,
-      applicant: {
-        id: shift.workerUser.id,
-        firstName: shift.workerUser.firstName,
-        lastName: shift.workerUser.lastName,
-        stars: shift.workerUser.workerData.stars,
-        totalReviews: shift.workerUser.workerData.totalReviews,
-        profileUrl: profileImg,
-      },
-      offer: {
-        id: shift.offer.id,
-        title: shift.offer.title,
-        from: shift.offer.from,
-        to: shift.offer.to,
-        usdHour: shift.offer.usdHour,
-        createAt: shift.offer.createAt,
-        address: {
-          id: shift.offer.address.id,
-          address: shift.offer.address.address,
-          city: shift.offer.address.city,
-          state: shift.offer.address.state,
-          postalCode: shift.offer.address.postalCode,
-        },
-        employerUser: {
-          userImages: shift.offer.employerUser.userImages,
-          employerData: {
-            description: shift.offer.employerUser.employerData.description,
-          },
-        },
-      },
-    };
-    return formatShift;
+    return shift;
   }
 
   async findByWorkerUserId(workerUserId: string, pagination?: PaginationDto) {
